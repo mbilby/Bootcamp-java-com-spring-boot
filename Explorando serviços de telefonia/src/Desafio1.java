d076e2f0c0a9a9c50225ffe1cb58444a4e79df59
Marcelo Barbosa <marcelo.barbosa@b2.solutions>
2024-07-26 16:28:41 -0300

    enviando desafio 2 de explorando telefonia

--- Explorando serviços de telefonia/src/Desafio1.java
+++ Explorando serviços de telefonia/src/Desafio1.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public class Desafio1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
